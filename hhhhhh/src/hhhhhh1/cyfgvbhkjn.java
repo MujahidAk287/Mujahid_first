a5be52ff1862e364b4f8b800883cbb9f18e99487
Mujahid <Mujahid@DESKTOP-3T42446>
2024-07-14 12:34:10 +0530

    New Project

---
+++ hhhhhh/src/hhhhhh1/cyfgvbhkjn.java
@@ -0,0 +1,22 @@
+package hhhhhh1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class cyfgvbhkjn {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "H:\\New Eclipse\\chromedriver-win64\\chromedriver.exe");
+		
+		ChromeOptions option= new ChromeOptions();
+		option.setBinary("H:\\New Eclipse\\chrome-win64\\chrome.exe");
+		WebDriver driver = new ChromeDriver();
+       driver.get("https://www.google.com");
+      
+       
+
+	}
+
+}
